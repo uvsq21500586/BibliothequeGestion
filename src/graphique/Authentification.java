471fc411d1e7adee0d69e6160b96a1ac6f2f7fd6
Yoann Volpi <yoann.volpi@live.fr>
2021-11-30 13:00:42 +0100

    Mon 1er commit

---
+++ src/graphique/Authentification.java
@@ -0,0 +1,43 @@
+package graphique;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Authentification {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Authentification window = new Authentification();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Authentification() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
